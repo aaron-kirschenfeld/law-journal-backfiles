976857363d3379f9fe2ee630bacc7d4e8bae7dea
Aaron Kirschenfeld <kirschen@email.unc.edu>
2017-04-18 16:00:26 -0400

    Update fpage.py

--- fpage.py
+++ fpage.py
@@ -1,7 +1,7 @@
 handle = open("fpage_prep.txt")
 for line in handle :
     line = line.rstrip()
-    stpt = line.find("Inst. ") + 6
+    stpt = line.find("Inst. ") + 6 # this is the line that would benefit from regex most likely  
     endpt = line.find("(")
     line = line[stpt:endpt]
     print line
