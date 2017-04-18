24ae6b88b7c6a815ca24cce5b03c3b1c79ee3ca7
Aaron Kirschenfeld <kirschen@email.unc.edu>
2017-04-18 16:01:44 -0400

    Update fpage.py

--- fpage.py
+++ fpage.py
@@ -1,7 +1,7 @@
 handle = open("fpage_prep.txt")
 for line in handle :
     line = line.rstrip()
-    stpt = line.find("Inst. ") + 6 # this is the line that would benefit from regex most likely  
+    stpt = line.find("Inst. ") + 6 # this is the line that would benefit from regex most likely; here, it's set to find from the text "Inst.", which was the first journal I used this on.  
     endpt = line.find("(")
     line = line[stpt:endpt]
     print line
